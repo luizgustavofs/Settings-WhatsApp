e713d9210c7a67ed14b3104e051eeadd5d6979ab
Luiz Gustavo <luizgustavofernandes16@gmail.com>
2022-01-31 11:59:47 -0300

    💄 Add styled components for StorageData

---
+++ src/Pages/StorageData/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  background: #fff;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  font-size: 40px;
+
+  font-weight: bold;
+
+`;
