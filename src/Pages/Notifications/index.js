b92f7196da3fb7b1e1c1f32a46a983e6622da5b4
Luiz Gustavo <luizgustavofernandes16@gmail.com>
2022-01-31 11:57:56 -0300

    ✨ Add src Notifications

---
+++ src/Pages/Notifications/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container, Title } from './styles';
+
+const Notifications = () => {
+  return <Container>
+    <Title>Notificationss</Title>
+  </Container>;
+}
+
+export default Notifications;
