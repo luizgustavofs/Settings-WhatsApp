40195a4ceffa53a52e182df46bca28b1af56a64d
Luiz Gustavo <luizgustavofernandes16@gmail.com>
2022-01-31 11:59:15 -0300

    ✨ Add src StarredMessages

---
+++ src/Pages/StarredMessage/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container, Title } from './styles';
+
+const StarredMessage = () => {
+  return <Container>
+    <Title>Starred Messages</Title>
+  </Container>;
+}
+
+export default StarredMessage;
