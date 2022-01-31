44bad041fa8f1e1053755ed18ac4e18f91c25021
Luiz Gustavo <luizgustavofernandes16@gmail.com>
2022-01-31 11:57:19 -0300

    ✨ Add src LinkedMessages

---
+++ src/Pages/LinkedMessages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container, Title } from './styles';
+
+const LinkedMessages = () => {
+  return <Container>
+    <Title>Linked Messages</Title>
+  </Container>;
+}
+
+export default LinkedMessages;
