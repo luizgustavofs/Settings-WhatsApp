e9595697997cedba6062d2ccd2d7fa7231895af2
Luiz Gustavo <luizgustavofernandes16@gmail.com>
2022-01-31 11:58:37 -0300

    ✨ Add src Payments

---
+++ src/Pages/Payments/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container, Title } from './styles';
+
+const Payments = () => {
+  return <Container>
+    <Title>Paymentss</Title>
+  </Container>;
+}
+
+export default Payments;
