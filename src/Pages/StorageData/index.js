5e408fce762de66a415fc9861998177fa46923fd
Luiz Gustavo <luizgustavofernandes16@gmail.com>
2022-01-31 11:59:38 -0300

    ✨ Add src StorageData

---
+++ src/Pages/StorageData/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container, Title } from './styles';
+
+const StorageData = () => {
+  return <Container>
+    <Title>Storage and Data</Title>
+  </Container>;
+}
+
+export default StorageData;
