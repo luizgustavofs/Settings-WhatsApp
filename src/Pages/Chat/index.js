5d3ab97232e588f40dd05c5481ccb7d7ce8b2d1a
Luiz Gustavo <luizgustavofernandes16@gmail.com>
2022-01-31 11:44:37 -0300

    ✨ Add src Chat

---
+++ src/Pages/Chat/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container, Title } from './styles';
+
+const Chat = () => {
+  return <Container>
+    <Title>Chats</Title>
+  </Container>;
+}
+
+export default Chat;
