e9456f8eb078bbef38a0d94d09e9800248b13a8f
Luiz Gustavo <luizgustavofernandes16@gmail.com>
2022-01-31 11:41:14 -0300

    ✨ Add src Account

---
+++ src/Pages/Account/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container, Title } from './styles';
+
+const Account = () => {
+  return <Container>
+    <Title>Account</Title>
+  </Container>;
+}
+
+export default Account;
