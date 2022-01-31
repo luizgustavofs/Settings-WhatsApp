46bd256cf24aeaf97ea277e70badafd759bd4f67
Luiz Gustavo <luizgustavofernandes16@gmail.com>
2022-01-31 11:56:19 -0300

    :sparkles: Add src Help

---
+++ src/Pages/Help/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container, Title } from './styles';
+
+const Help = () => {
+  return <Container>
+    <Title>Help</Title>
+  </Container>;
+}
+
+export default Help;
