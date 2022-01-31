61bd8bf82de8cbf33b1e8070f2cde2a9d8050502
Luiz Gustavo <luizgustavofernandes16@gmail.com>
2022-01-31 12:00:02 -0300

    ✨ Add src TellFriend

---
+++ src/Pages/TellFriend/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container, Title } from './styles';
+
+const TellFriend = () => {
+  return <Container>
+    <Title>Tell a Friend</Title>
+  </Container>;
+}
+
+export default TellFriend;
